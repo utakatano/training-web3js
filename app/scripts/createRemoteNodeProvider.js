8068edf9e68f64dc2398af383323d9e064d710df
Kei Yagi <keiyagi@Keis-MacBook-Air.local>
2021-10-06 23:52:15 +0900

    create-local-remote-example

---
+++ app/scripts/createRemoteNodeProvider.js
@@ -0,0 +1,6 @@
+// https://web3js.readthedocs.io/en/v1.5.2/web3.html#example-remote-node-provider
+import Web3 from 'web3'
+
+const web3 = new Web3('https://eth-mainnet.alchemyapi.io/v2/your-api-key')
+
+console.log(web3)
